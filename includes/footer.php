ccd53e315f749eb64b672379da4dcdfe239f28a3
Yulia Wulandari <yuliawulandari271@gmail.com>
2022-04-22 10:08:01 +0700

    UPDATE TAMPILAN

--- includes/footer.php
+++ includes/footer.php
@@ -1,36 +1,27 @@
 <!-- Footer -->
-<footer class="footer">
-        <div class="container">
-            <nav class="pull-left">
-                <ul class="nav">
-                    <li class="nav-item">
-                        <a class="nav-link">
-                            <br>
-                        </a>
-                    </li>
-					<li class="nav-item">
-                        <a href="#" class="logo d-flex align-items-center">
-                            <img src="assets/img/ILO1.png" height="50" alt="navbar brand" class="">
-                        </a>
-                    </li>
-					<li class="nav-item">
-                        <a href="#" class="logo d-flex align-items-center">
-                            <img src="assets/img/Indonesia.png" style="margin-left: 12px;" height="50" alt="navbar brand" class="">
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a href="#" class="logo d-flex align-items-center">
-                            <img src="assets/img/BEJ.png" height="70" alt="navbar brand" class="">
-                        </a>
-                    </li>
-                    
-                </ul>
-            </nav>
-            <div class="copyright ml-auto">
-                2021, made with <i class="fa fa-heart heart text-danger"></i> by <a href="#">PSTeam</a>
-            </div>
-        </div>
-    </footer>
+<footer style="background-color: white; padding: 10px; border-top: 1px solid #eee; padding-top: 20px;">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-4 my-auto" style="display: flex; flex-direction: row;">
+						<a href="">
+							<img src="assets/img/ILO1.png" height="50" alt="navbar brand" class="">
+						</a>
+						<a href="#" class="logo d-flex align-items-center">
+							<img src="assets/img/Indonesia.png" style="margin-left: 12px;" height="50" alt="navbar brand" class="">
+						</a>
+						<a href="#" class="logo d-flex align-items-center">
+							<img src="assets/img/BEJ.png" height="70" alt="navbar brand" class="">
+						</a>
+					</div>
+					<div class="col-md-5"></div>
+					<div class="col-md-3">
+						<div class="copyright ml-auto">
+							2021, made with <i class="fa fa-heart heart text-danger"></i> by <a href="#">PSTeam</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</footer>
 	<!-- End Footer -->
 	</div>
 </body>
